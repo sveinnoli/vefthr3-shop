1bc5dc985c89a3dd9a97cbfbb0d1702f4f0c88bb
Sveinn oli Gudmundsson <sveinnoli@hotmail.com>
2020-04-02 02:50:21 +0000

    POST GET REDIRECT

--- 1test.py
+++ 1test.py
@@ -1,39 +1,43 @@
-import os
+import os, random, math
+from random import randint
 
-test_dict = {"wares":[{"product": "shoes", "price": "3500", "image": "~/{{usr}}/bin/img/s.jpg", "id": os.urandom(10).hex()},
-            {"product": "jacket", "price": "5500", "image": "image", "id": os.urandom(10).hex()}]}
+#pythagoras = lambda a, b: math.pow(a,2)+math.pow(b,2)
+#print(format(pythagoras(3,3), ".2f"))
 
-for item in test_dict["wares"]:
-    for key, value in item.items():
-        print(key, value)
+data = {}
+data["results"] = [{"item1":"item", "item2":"item", "item3":"item", "price":20, "cost":10000},
+                {"item1":"item", "item2":"item", "item3":"item", "price":25, "cost": 200},
+                {"item1":"item", "item2":"item", "item3":"item", "price":1, "cost":1000}]
+dataset = data["results"]
+    
+def find_min_verd(dict_list):
+    return {"price":min(dict_list, key=lambda d: d.get("price", float('inf')))["price"]}
+
+print(find_min_verd(dataset))
 
 
+lst = [{'price': 99, 'barcode': '2342355'}, {'price': 88, 'barcode': '2345566'}]
 
+#returns dict that has lowest and highest value of a (key, value) pair
+minPricedItem = min(lst, key=lambda x: x.get(x["price"], float("inf")))["price"]
+maxPricedItem = max(lst, key=lambda x:x['price'])
+print("min:", minPricedItem, "\nmax:",maxPricedItem)
 
-#@app.route('/on')
-#def sessionon():
-#    if "testing" in session:
-#        text="Session already ON"
-#    else:
-#        session['testing'] = 'working'
-#        text = "Session is now SET"
-#    return render_template("shop.html", text=text, session=session)
-#
-## Deletes session
-#@app.route('/off')
-#def sessionoff():
-#    if "wares" in session:
-#        session.clear()
-#        text="Session cleared"
-#    else:
-#        text="Session was not set"
-#    return render_template("shop.html", text=text)
-#
-## Checks for session
-#@app.route('/check')
-#def checksession():
-#    if 'wares' in session:
-#        text = session
-#    else:
-#        text="OFF"
-#    return render_template("shop.html", text=text)
+#if "wares" not in session:
+#        session["wares"] = []
+#    if request.method == 'POST': #Catches submit form. 
+#        product_id = request.form['add_to_cart']
+#        for item in products["wares"]:
+#            for key, value in item.items():
+#                if key == "id" and value == product_id:
+#                    #Bug in session caching dosen't allow you to append it, recreate session instance and append it back to session
+#                    temp_session = session["wares"]
+#                    temp_session.append(item)
+#                    session["wares"] = temp_session
+#                    print(session["wares"])     
+#                if key == "id" and value == product_id:
+                    #Bug in session caching dosen't allow you to append it, recreate session instance and append it back to session
+                    #temp_session = session["wares"]
+                    #temp_session.append(item)
+                    #session["wares"] = temp_session
+                    #print(session["wares"])     
--- app.py
+++ app.py
@@ -1,23 +1,44 @@
-from flask import Flask, session, render_template, request
 import os
+from flask import Flask, session, render_template, request, redirect
 app = Flask(__name__)
 
 #Session key
 app.config['SECRET_KEY'] = os.urandom(24).hex()
 
-#products in shop 
 #NOTE keep prices in int, figure out another time how to convert it from str to int
+
+#Add type for shoes and product for product name e.g: shirt, wizliz or something of the sort
 products = {"wares":[
             {"price":4000, "product":"shirt", "image":"/static/images/shirt1.jpg","id":os.urandom(10).hex()},
             {"price":3500, "product":"shoes", "image":"/static/images/shoes1.jpg","id":os.urandom(10).hex()},
             {"price":5000, "product":"pants", "image":"/static/images/pants1.jpg","id":os.urandom(10).hex()},
-            {"price":7500, "product":"shoes", "image":"/static/images/shoes2.jpg","id":os.urandom(10).hex()}
+            {"price":7500, "product":"shoes", "image":"/static/images/shoes2.jpg","id":os.urandom(10).hex()},
             ]}
-
+#functions
+def add_to_cart(product_id):
+    for item in products["wares"]:
+        for key, value in item.items():
+            if key == "id" and value == product_id:
+                #Bug in session caching dosen't allow you to append it, recreate session instance and append it back to session
+                temp_session = session["wares"]
+                temp_session.append(item)
+                session["wares"] = temp_session
 
 @app.route('/', methods=['GET','POST'])
-def index():
-    return render_template("shop.html", products=products, session=session)
+def store():
+    if "wares" not in session:
+        session["wares"] = []
+    if request.method == 'POST': #Catches submit form. 
+        product_id = request.form['add_to_cart']
+        add_to_cart(product_id)           
+        return render_template("shop.html", products=products, session=session)
+    else:
+        return render_template("shop.html", products=products, session=session)
+
+@app.route('/checkout', methods=['GET','POST'])
+def checkout():
+    return render_template("checkout.html", session=session)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
